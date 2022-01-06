7caf1b2ce3824eeddfc3c0d5822e091ad29ca9a5
Robert Peszek <peszeks@gmail.com>
2022-01-05 21:24:57 -0700

    ts-notes p4 examples

--- ts-notes/src/Part4.ts
+++ ts-notes/src/Part4.ts
@@ -10,37 +10,46 @@ export const bodyScope = <T>(value: T | undefined | null): void => {
         const t2: T = {} as any
         const myFn: (_:T) => void = _ => {}
     }
-  }
+}
 
+// export const bodyScope2: <T>(_: T | undefined | null) => void = value => {
+//     if(value) {
+//         const t: T = value
+//     }
+// }
 
 // type lever unknown guards
 
-type IsUnknown<T> = unknown extends T? 1: 0
+type IsUnknown<T> = unknown extends T? true: false
 
 export function isUnknown<T>(t: T): IsUnknown<T> {
-   return 1 as any //incorrect runtime value, but we do not care
+   return true as any //incorrect runtime value, but we do not care
 }
 
 const unk: unknown = {}
 const test: IsUnknown<typeof unk> = {} as any
-const test2: 1 = isUnknown(unk)
+const test2: true = isUnknown(unk)
 
 
 export function verifyUnknown<T>(p: IsUnknown<T>, t: T): T {
     return t
 }
 
-verifyUnknown(1, unk)
+
 //does not compile
-//verifyUnknown(0, unk)
+verifyUnknown(false, "test")
+//does not compile
+//verifyUnknown(false, unk)
+verifyUnknown(true, unk)
 
 const whyWhyWhy = async (item: Office.MessageRead): Promise<void> => {
     
-    const body =  verifyUnknown(0, await officePromise (curry(item.body.getAsync)(Office.CoercionType.Html)))
+    const body =  verifyUnknown(false, await officePromise (curry(item.body.getAsync)(Office.CoercionType.Html)))
 
     const crazyConfig : (_: Office.AsyncResult<string>) => void = x => ""
     
-    const body4 = verifyUnknown(1, await officePromise (curry3(item.body.getAsync)(Office.CoercionType.Html)(crazyConfig))) 
+    //note need to put true to compile
+    const body4 = verifyUnknown(true, await officePromise (curry3(item.body.getAsync)(Office.CoercionType.Html)(crazyConfig))) 
 }  
 
 export const verifyUnknownCallback: <T, R>(p: IsUnknown<T>, fn: (_: T) => R) => (_: T) => R 
@@ -48,11 +57,13 @@ export const verifyUnknownCallback: <T, R>(p: IsUnknown<T>, fn: (_: T) => R) => 
    return fn
 }
 
-verifyUnknownCallback(1, curry(curry)({} as any))
+//note need to put true to compile
+verifyUnknownCallback(true, curry(curry)({} as any))
 
 
 // --- higher rank
 
+//examples with generic callbacks
 declare function fn1<T> (f:(t:T)=> void): void 
 declare function fn2(f: <T>(t:T)=> void): void 
 
@@ -65,6 +76,8 @@ fn1(useStr)
 //fn2(useStr)
 fn2(getCallback)
 
+
+//examples using generic thunks
 declare function fn3<P,T> (f:()=> T): void 
 declare function fn4(f: <T>() => T): void
 
@@ -74,7 +87,7 @@ declare function typeHole<T>():T
 
 
 fn3(strThunk)
-//fn4(strThunk, numThank)
+//fn4(strThunk)
 fn4(typeHole)
 
 //Factory pattern replacement
@@ -88,10 +101,13 @@ class MyFoo implements Foo{
     }
 }
 
-function existencialFactory(t: <T extends Foo>(t:T) => void): void {
+function existencialFactory(t: <T extends Foo>(_:T) => void): void {
     t(new MyFoo())
 }
-function nonExistencialFactory<T extends Foo> (t:(t:T) => void): void {
+function nonExistencialFactory<T extends Foo> (t:(_:T) => void): void {
+    //Compilation Error
+    //Argument of type 'MyFoo' is not assignable to parameter of type 'T'.
+    // 'MyFoo' is assignable to the constraint of type 'T', but 'T' could be instantiated with a different subtype of constraint 'Foo'.ts(2345)
     //t(new MyFoo()) //does not compile
 }
 
@@ -140,12 +156,18 @@ const goodProgram = <Password>(p: Password): string[] => {
 
 const stealPassword = <Password>(p: Password): Password => p
 
-const valid = verifyUnknown(0, secretive(goodProgram)) //valid: string[]
+
+secretive(goodProgram)
+secretive(stealPassword)
+
+const valid = verifyUnknown(false, secretive(goodProgram)) //valid: string[]
 const invalid_ = secretive(stealPassword) //invalid: unknown, unfortunately compiles retrieving password as unknown
 
-//const invalid = verifyUnknown(0,secretive(stealPassword)) //does not compile!
+//const invalid = verifyUnknown(false,secretive(stealPassword)) //does not compile!
 
-// interface attempt also allows type escape
+
+
+// interface based approach
 interface ExistentialCallback<R> {
     <T>(t: T): R;
   }
@@ -161,32 +183,94 @@ const stealPassword2 : ExistentialCallback<unknown> = stealPassword
 secretive2(stealPassword2)
 
 
- 
+//-- preventing subtyping
+
+type Same<P,T> = P extends T? (T extends P? true: false): false
+
+type Hello = {hello: string}
+
+const proofHello = <T> (t: T): Same<Hello,T> => {
+    return {} as any //does not matter
+ }
+
+//hover over to see 'const phello1: true' and 'const phello2: false'
+const phello1 = proofHello({hello: "world"})
+const phello2 = proofHello({hello: "world", since : 2020})
+
+
+
+const verifyHello = <T> (_: Same<Hello,T>, t:T): T => t
+
+verifyHello(true, {hello: "world"})
+verifyHello(false, {hello: "world", since : 2020})
+
+const verifySame = <P> () => <T> (_: Same<P,T>, t:T): T => t
+
+verifySame<Hello>()(true, {hello: "world"})
+verifySame<Hello>()(false, {hello: "world", since : 2020})
+//verifySame<Hello>()(true, {hello: "world", since : 2020})
+
+//Note to get safety I end up with casting
+const safePush = <P, T> (_: Same<P,T>, ps: P[], t: T): number => ps.push(t as any)
+
+
+const intlist: number[] = [1,2,3]
+const list: unknown[] = intlist
+list.push("not a number") //unsafe TS push
+
+safePush(true, intlist, 1)  //this is safe
+safePush(false, list, 1)    //this is risky and will not compile with true
+safePush(false, list, "not a number") //this is risky (here wrong) and will not compile with true
+
+//This is easier on TS to type check than a tuple
+type Pair<A,B> = {fst: A, snd: B}
+
+//--- attempt to crate existential safePush with only one top level type variable
+const safePush2 = <P> (ps: P[], fn: <T> () => Pair<Same<P,T>, T>): number => {
+    const res = fn().snd
+    return ps.push(res as any)
+}
+
+//Sadly, this does not want to compile
+// Note this is still rank-2 (not higher), the position of quantification counts, 
+// not the position of quantified variable
+
+// safePush2(intlist, () => {
+//     return {fst: true, snd: 1}
+// })
+
+
 
 // --- Phantom types
 
 // People use classes / interfaces to accomplish similar things
 // see also https://github.com/microsoft/TypeScript/issues/21625
 
-type Person0<T> = {firstNm: string, lastNm: string}  //adding value level representation t: T as a filed would fix this
+type Person<T> = {firstNm: string, lastNm: string}  //adding value level representation `phantom?: T` 
 
-const testP0 : <T>(fst: string, lst: string) => Person0<T> = (fst, lst) => {
+const createPerson : <T>(fst: string, lst: string) => Person<T> = (fst, lst) => {
     return {firstNm: fst, lastNm: lst}
 } 
 
-type NotValidated = {type: "notvalidated"}
 type Validated = {type: "validated"}
+type ValidationError = string
 
-declare function validate0(p: Person0<NotValidated>):  Person0<Validated> 
+declare function validate<T>(p: Person<T>):  ValidationError | Person<Validated> 
 
-declare function doSomethingValidated0(p: Person0<Validated>): void
+declare function doSomethingValidated(p: Person<Validated>): void
 
-function validatedOrNot0<T>(p:Person0<T>): void{
-    doSomethingValidated0(p)
+function validatedOrNot<T>(p:Person<T>): void{
+    doSomethingValidated(p)
 }
 
-function notValidated0 (p:Person0<NotValidated>): void{
-    doSomethingValidated0(p)
+type ClearlyNotValidated = {type: "notvalidated"}
+
+function notValidated (p:Person<ClearlyNotValidated>): void {
+    doSomethingValidated(p)
+}
+
+function validated(p:Person<Validated>): void {
+    doSomethingValidated(p)
 }
 
 
@@ -199,7 +283,7 @@ const testP1 : <T>(fst: string, lst: string) => Person1<T> = (fst, lst) => {
     return {firstNm: fst, lastNm: lst}
 } 
 
-declare function validate1(p: Person1<NotValidated>):  Person1<Validated> 
+declare function validate1<T>(p: Person1<T>):  ValidationError | Person1<Validated> 
 
 declare function doSomethingValidated1(p: Person1<Validated>): void
 
@@ -209,7 +293,7 @@ declare function doSomethingValidated1(p: Person1<Validated>): void
 //     doSomethingValidated1(p)
 // }
 
-// function notValidated1 (p:Person1<NotValidated>): void{
+// function notValidated1 (p:Person1<ClearlyNotValidated>): void{
 //     doSomethingValidated1(p)
 // }
 
@@ -217,78 +301,24 @@ declare function doSomethingValidated1(p: Person1<Validated>): void
 // verifyExtends<Person1<number>, Person1<1>>()
 
 
-
-
-
-//-- never vs type hole
-
-export const _never = (): never => {
-    throw new Error("hole"); 
+interface Comparator<T> {
+    compare (o1: T, o2: T): number
 }
 
-export const _hole = <T>(): T =>  _never() 
-
-
-export const __hole = <T>(): T => {
-    throw new Error("hole"); 
+class MyClass implements Comparator<MyClass> {
+    num: number
+    constructor (num: number) {this.num = num}
+    compare (o1: MyClass, o2: MyClass): number {return o1.num - o2.num}
 }
 
-export const __never = (): never => __hole()
+type SortStatus = "ascending" | "descending" | "none"
+type List<T, Status> = {type: "empty"} | {type: "cons", head: T, tail: List<T, Status>}
+
+declare function sortAscending<T extends Comparator<T>, Status>(list: List<T, Status>):  List<T, "ascending">
+
+declare function doSomethingWithSortedList<T extends Comparator<T>>(list: List<T, "ascending">): void
+
+const testList : List<MyClass, unknown> = _()
+sortAscending(testList)
 
 
-
-// ---- `never` note
-
-const nevr : never = _()
-
-
-type SameAs<A> = Either<never,A>
-
-const onlyA: SameAs<number> = {type: "right", content:_()}
-const impossible: SameAs<number> = {type: "left", content:_()}
-
-// --- undefined callbacks
-
-declare function someUnknownCallback(t: unknown): void 
-const overbar: <T>(_:T) => void =  someUnknownCallback
-
-declare function someOverbar<T>(t:T): void
-const unknownCallback: (_: unknown) => void = someOverbar
-
-const overbar2: <T>(_:T) => void = t => {}
-const unknownCallback2: (_: unknown) => void = t => {}
-
-overbar(_() === _())
-overbar("" + _())
-
-
-const overbar3: <T>(_:T) => void =  _()
-//export const underbar: <T>() => T = someOverbar //this obviously does not compile
-
-// declare function unk<R>(t: unknown): R 
-// const top = <T,R>(t:T): R => unk(t)
-
-// declare function top2<T,R>(t:T): R
-// const unk2 = <R>(t: unknown): R => top(t)
-
-
-
-//Extras
-//classes are structually typed too
-class Bye {
-    constructor() {
-        this.bye = "1"
-    }
-    bye: string
-}
-
-class Bye2 {
-    constructor() {
-        this.bye = "2"
-    }
-    bye: string 
-}
-
-declare function useBye(b: Bye): void 
-
-useBye(new Bye2())
